12c9ddd7d82743ed8b6ab766fd682210e290d8df
VladimirV94 <vladimirvolohovich@yandex.ru>
2025-07-19 06:41:19 +0300

    Разбиение теста на более мелкие

--- src/test/java/todogroup/todoproject/controller/TaskControllerTest.java
+++ src/test/java/todogroup/todoproject/controller/TaskControllerTest.java
@@ -109,54 +109,66 @@ class TaskControllerTest {
 	}
 
 	@Test
-	void sortAllTasks() {
+	void sortAllTasks_shouldSortByDeadLineDirect() {
+		final TaskResponseDTO responseTaskDTOByDeadline1 = Mockito.mock(TaskResponseDTO.class);
+		final TaskResponseDTO responseTaskDTOByDeadline2 = Mockito.mock(TaskResponseDTO.class);
+		List<TaskResponseDTO> sourceByDeadlineDirect = List.of(responseTaskDTOByDeadline1, responseTaskDTOByDeadline2);
+		Mockito.when(taskService.sortAllTasksByDeadline(SortDirection.DIRECT)).thenReturn(sourceByDeadlineDirect);
+
+		List<TaskResponseDTO> resultByDeadlineDirect = taskController.sortAllTasks(SortBy.DEADLINE, SortDirection.DIRECT);
+
+		assertNotNull(resultByDeadlineDirect);
+		assertEquals(sourceByDeadlineDirect.size(), resultByDeadlineDirect.size());
+		assertEquals(sourceByDeadlineDirect.get(0), resultByDeadlineDirect.get(0));
+		assertEquals(sourceByDeadlineDirect.get(1), resultByDeadlineDirect.get(1));
+	}
+
+	@Test
+	void sortAllTasks_shouldSortByDeadLineReverse() {
+		final TaskResponseDTO responseTaskDTOByDeadline1 = Mockito.mock(TaskResponseDTO.class);
+		final TaskResponseDTO responseTaskDTOByDeadline2 = Mockito.mock(TaskResponseDTO.class);
+		List<TaskResponseDTO> sourceByDeadlineReverse = List.of(responseTaskDTOByDeadline2, responseTaskDTOByDeadline1);
+		Mockito.when(taskService.sortAllTasksByDeadline(SortDirection.REVERSE)).thenReturn(sourceByDeadlineReverse);
+
+		List<TaskResponseDTO> resultByDeadlineReverse = taskController.sortAllTasks(SortBy.DEADLINE, SortDirection.REVERSE);
+
+		assertNotNull(resultByDeadlineReverse);
+		assertEquals(sourceByDeadlineReverse.size(), resultByDeadlineReverse.size());
+		assertEquals(sourceByDeadlineReverse.get(0), resultByDeadlineReverse.get(0));
+		assertEquals(sourceByDeadlineReverse.get(1), resultByDeadlineReverse.get(1));
+	}
+
+	@Test
+	void sortAllTasks_shouldSortByStatusDirect() {
 		final TaskResponseDTO responseTaskDTOByStatus1 = Mockito.mock(TaskResponseDTO.class);
 		final TaskResponseDTO responseTaskDTOByStatus2 = Mockito.mock(TaskResponseDTO.class);
 		final TaskResponseDTO responseTaskDTOByStatus3 = Mockito.mock(TaskResponseDTO.class);
-		final TaskResponseDTO responseTaskDTOByDeadline1 = Mockito.mock(TaskResponseDTO.class);
-		final TaskResponseDTO responseTaskDTOByDeadline2 = Mockito.mock(TaskResponseDTO.class);
-
 		List<TaskResponseDTO> sourceByStatusDirect = List.of(responseTaskDTOByStatus1, responseTaskDTOByStatus2, responseTaskDTOByStatus3);
-		List<TaskResponseDTO> sourceByStatusReverse = List.of(responseTaskDTOByStatus3, responseTaskDTOByStatus2, responseTaskDTOByStatus1);
-		List<TaskResponseDTO> sourceByDeadlineDirect = List.of(responseTaskDTOByDeadline1, responseTaskDTOByDeadline2);
-		List<TaskResponseDTO> sourceByDeadlineReverse = List.of(responseTaskDTOByDeadline2, responseTaskDTOByDeadline1);
-
 		Mockito.when(taskService.sortAllTasksByStatus(SortDirection.DIRECT)).thenReturn(sourceByStatusDirect);
-		Mockito.when(taskService.sortAllTasksByStatus(SortDirection.REVERSE)).thenReturn(sourceByStatusReverse);
-		Mockito.when(taskService.sortAllTasksByDeadline(SortDirection.DIRECT)).thenReturn(sourceByDeadlineDirect);
-		Mockito.when(taskService.sortAllTasksByDeadline(SortDirection.REVERSE)).thenReturn(sourceByDeadlineReverse);
-
-
 
 		List<TaskResponseDTO> resultByStatusDirect = taskController.sortAllTasks(SortBy.STATUS, SortDirection.DIRECT);
-		List<TaskResponseDTO> resultByStatusReverse = taskController.sortAllTasks(SortBy.STATUS, SortDirection.REVERSE);
-		List<TaskResponseDTO> resultByDeadlineDirect = taskController.sortAllTasks(SortBy.DEADLINE, SortDirection.DIRECT);
-		List<TaskResponseDTO> resultByDeadlineReverse = taskController.sortAllTasks(SortBy.DEADLINE, SortDirection.REVERSE);
-
-
 
 		assertNotNull(resultByStatusDirect);
-		assertNotNull(resultByStatusReverse);
-		assertNotNull(resultByDeadlineDirect);
-		assertNotNull(resultByDeadlineReverse);
-
 		assertEquals(sourceByStatusDirect.size(), resultByStatusDirect.size());
-		assertEquals(sourceByStatusReverse.size(), resultByStatusReverse.size());
-		assertEquals(sourceByDeadlineDirect.size(), resultByDeadlineDirect.size());
-		assertEquals(sourceByDeadlineReverse.size(), resultByDeadlineReverse.size());
-
 		assertEquals(sourceByStatusDirect.get(0), resultByStatusDirect.get(0));
 		assertEquals(sourceByStatusDirect.get(1), resultByStatusDirect.get(1));
 		assertEquals(sourceByStatusDirect.get(2), resultByStatusDirect.get(2));
+	}
 
+	@Test
+	void sortAllTasks_shouldSortByStatusReverse() {
+		final TaskResponseDTO responseTaskDTOByStatus1 = Mockito.mock(TaskResponseDTO.class);
+		final TaskResponseDTO responseTaskDTOByStatus2 = Mockito.mock(TaskResponseDTO.class);
+		final TaskResponseDTO responseTaskDTOByStatus3 = Mockito.mock(TaskResponseDTO.class);
+		List<TaskResponseDTO> sourceByStatusReverse = List.of(responseTaskDTOByStatus3, responseTaskDTOByStatus2, responseTaskDTOByStatus1);
+		Mockito.when(taskService.sortAllTasksByStatus(SortDirection.REVERSE)).thenReturn(sourceByStatusReverse);
+
+		List<TaskResponseDTO> resultByStatusReverse = taskController.sortAllTasks(SortBy.STATUS, SortDirection.REVERSE);
+
+		assertNotNull(resultByStatusReverse);
+		assertEquals(sourceByStatusReverse.size(), resultByStatusReverse.size());
 		assertEquals(sourceByStatusReverse.get(0), resultByStatusReverse.get(0));
 		assertEquals(sourceByStatusReverse.get(1), resultByStatusReverse.get(1));
 		assertEquals(sourceByStatusReverse.get(2), resultByStatusReverse.get(2));
-
-		assertEquals(sourceByDeadlineDirect.get(0), resultByDeadlineDirect.get(0));
-		assertEquals(sourceByDeadlineDirect.get(1), resultByDeadlineDirect.get(1));
-
-		assertEquals(sourceByDeadlineReverse.get(0), resultByDeadlineReverse.get(0));
-		assertEquals(sourceByDeadlineReverse.get(1), resultByDeadlineReverse.get(1));
 	}
 }
